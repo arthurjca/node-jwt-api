ac4d1cd00a81f769dde143ff4d66027d60a6f676
Arthur Alves <arthur.alves-ext@ab-inbev.com>
2025-04-12 20:33:43 -0300

    first real commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');  
+const app = express();  
+const { getAllUsers, createUser } = require('./src/controllers/userController');  
+
+app.use(express.json());  
+
+app.get('/', (req, res) => {  
+  res.send('API Node.js + JWT');  
+});  
+app.get('/users', getAllUsers);  
+app.post('/users', createUser);
+
+const PORT = 3000;  
+app.listen(PORT, () => {  
+  console.log(`Servidor rodando em http://localhost:${PORT}`);  
+});  
---
+++ src/controllers/userController.js
@@ -0,0 +1,14 @@
+const users = [];  
+
+const getAllUsers = (req, res) => {  
+  res.json(users);  
+};  
+
+const createUser = (req, res) => {  
+  const { name, email } = req.body;  
+  const newUser = { id: users.length + 1, name, email };  
+  users.push(newUser);  
+  res.status(201).json(newUser);  
+};  
+
+module.exports = { getAllUsers, createUser };  
