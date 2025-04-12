9874db17bc557fa387664b8af65cbae13ffde663
Arthur Alves <arthurjca@gmail.com>
2025-04-12 20:37:43 -0300

    removing spaces

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');  
-const app = express();  
-const { getAllUsers, createUser } = require('./src/controllers/userController');  
+const express = require('express');
+const app = express();
+const { getAllUsers, createUser } = require('./src/controllers/userController');
 
-app.use(express.json());  
+app.use(express.json());
 
-app.get('/', (req, res) => {  
-  res.send('API Node.js + JWT');  
-});  
-app.get('/users', getAllUsers);  
+app.get('/', (req, res) => {
+  res.send('API Node.js + JWT');
+});
+app.get('/users', getAllUsers);
 app.post('/users', createUser);
 
-const PORT = 3000;  
-app.listen(PORT, () => {  
-  console.log(`Servidor rodando em http://localhost:${PORT}`);  
-});  
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,14 @@
-const users = [];  
+const users = [];
 
-const getAllUsers = (req, res) => {  
-  res.json(users);  
-};  
+const getAllUsers = (req, res) => {
+  res.json(users);
+};
 
-const createUser = (req, res) => {  
-  const { name, email } = req.body;  
-  const newUser = { id: users.length + 1, name, email };  
-  users.push(newUser);  
-  res.status(201).json(newUser);  
-};  
+const createUser = (req, res) => {
+  const { name, email } = req.body;
+  const newUser = { id: users.length + 1, name, email };
+  users.push(newUser);
+  res.status(201).json(newUser);
+};
 
-module.exports = { getAllUsers, createUser };  
+module.exports = { getAllUsers, createUser };
